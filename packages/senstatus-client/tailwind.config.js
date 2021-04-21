22f139732ebd0791937645c054e9ed95b5844548
Tim Durward <timothy.a.durward@gmail.com>
2021-04-21 00:51:09 -0700

    move to vite and typescript

--- packages/senstatus-client/tailwind.config.js
+++ packages/senstatus-client/tailwind.config.js
@@ -1,16 +1,24 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  mode: 'jit',
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    safeList: [],
+    content: ['./index.html', './src/**/*.tsx', './src/**/*.ts'],
+  },
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
-      },
+    minWidth: {
+      '40': '10rem',
+      '60': '15rem',
+      '80': '20rem',
+      '100': '25rem',
     },
+    maxWidth: {
+      '120': '30rem',
+      '160': '40rem',
+      '200': '50rem',
+    }
   },
-  variants: {
-    extend: {},
-  },
+  variants: {},
   plugins: [],
-};
+}
